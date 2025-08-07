46592c2dffb433dc60b0bd372b2acf1b816c45bb
Nitesh Purohit <nitesh.purohit.it@gmail.com>
2025-08-06 22:34:39 -0400

    chore: Standardize exception signaling and fix specs
    Updates the Dangerfile to use `fail` instead of `raise` for signaling exceptions, aligning with RuboCop style conventions.
    
    Also corrects grammar in gemspec test descriptions.

--- mat_views/spec/gemspec_spec.rb
+++ mat_views/spec/gemspec_spec.rb
@@ -3,11 +3,11 @@
 RSpec.describe 'mat_views.gemspec' do # rubocop:disable RSpec/DescribeClass
   subject(:spec) { Gem::Specification.load('mat_views.gemspec') }
 
-  it 'have mat_view name' do
+  it 'has mat_view name' do
     expect(spec.name).to eq('mat_views')
   end
 
-  it 'have mat_view version' do
+  it 'has mat_view version' do
     expect(spec.version).to eq(MatViews::VERSION)
   end
 end
