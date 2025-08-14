a475d971b0cb558959f43d6991d7e686e4011a8f
Nitesh Purohit <nitesh.purohit.it@gmail.com>
2025-08-13 21:51:31 -0400

    feat: Add Rails demo application for mat_views gem (#81)
    Introduces a new Rails 8 application, `mat_views_demo`, to serve as a comprehensive demonstration and testing environment for the `mat_views` gem.
    
    This application includes:
    - A full Rails project structure with models, migrations, and seed data.
    - CI configuration using GitHub Actions for automated testing and linting.
    - Deployment setup with Docker and Kamal.
    - Rake tasks to bootstrap the demo environment, including seeding data and defining materialized views.

---
+++ mat_views_demo/app/models/user.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Copyright Codevedas Inc. 2025-present
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+class User < ApplicationRecord
+  has_many :accounts, dependent: :destroy
+  has_many :events, dependent: :destroy
+  has_many :sessions, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: true # rubocop:disable Rails/UniqueValidationWithoutIndex
+end
---
+++ mat_views_demo/config/initializers/mat_views.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# Copyright Codevedas Inc. 2025-present
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+MatViews.configure do |config|
+  # Set the retry behavior for MatViews, globally
+  # This determines whether MatViews will retry failed refreshes.
+  # Defaults to true.
+  # Uncomment the line below to set retry behavior.
+  # config.retry_on_failure = true
+
+  ### Job Adapter Configuration
+  # Set job adapter for MatViews
+  # This can be set to :active_job, :sidekiq, and :resque,
+  # defaults to :active_job.
+  #
+  # Depending on your application's job processing setup.
+  # Uncomment the line below to set the job adapter.
+  # config.job_adapter = :active_job
+
+  # job_queue is the queue name for the job adapter.
+  # Default is :default.
+  # This is used to specify the queue where MatViews jobs will be enqueued.
+  # Uncomment the line below to set the job queue.
+  config.job_queue = :default
+end
