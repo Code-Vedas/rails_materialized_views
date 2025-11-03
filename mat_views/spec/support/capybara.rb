4844d654d2062ce3f6d52c196eff9bc227d87515
Nitesh Purohit <nitesh.purohit.it@gmail.com>
2025-11-03 18:57:21 -0500

    Adds support for remote Selenium execution
    Configures Capybara to connect to a remote Selenium instance when the `SELENIUM_REMOTE_URL` environment variable is set. This allows running feature specs in containerized environments.
    
    If the environment variable is not present, the setup defaults to using a local headless browser.

--- mat_views/spec/support/capybara.rb
+++ mat_views/spec/support/capybara.rb
@@ -16,15 +16,48 @@ def setup_with_selenium_manager
   driver.quit
 end
 
-setup_with_selenium_manager
-
 Capybara.server = :puma, { Silent: true }
 Capybara.default_max_wait_time = ENV.fetch('CAPYBARA_WAIT_TIME', 6).to_i
 Capybara.match = :smart
 Capybara.ignore_hidden_elements = true
 
-Capybara.default_driver = :selenium_headless
-Capybara.javascript_driver = :selenium_headless
+Capybara.register_driver :firefox_headless do |app|
+  opts = Selenium::WebDriver::Firefox::Options.new
+  opts.args << '-headless'
+  opts.add_preference('layout.css.devPixelsPerPx', '1.0')
+  driver = Capybara::Selenium::Driver.new(app, browser: :firefox, options: opts)
+  driver.browser.manage.window.resize_to(1920, 1080)
+  driver
+end
+
+Capybara.register_driver :remote_firefox do |app|
+  url = ENV.fetch('SELENIUM_REMOTE_URL', 'http://localhost:4444/wd/hub')
+  opts = Selenium::WebDriver::Firefox::Options.new
+  opts.args << '-headless'
+  opts.add_preference('layout.css.devPixelsPerPx', '1.0')
+  driver = Capybara::Selenium::Driver.new(
+    app,
+    browser: :remote,
+    url: url,
+    options: opts
+  )
+  driver.browser.manage.window.resize_to(1920, 1080)
+  driver
+end
+
+if ENV['SELENIUM_REMOTE_URL'].present?
+  Capybara.default_driver = :remote_firefox
+  Capybara.javascript_driver = :remote_firefox
+  Capybara.server_host = '0.0.0.0'
+  Capybara.server_port = ENV.fetch('CAPYBARA_PORT', '3000').to_i
+  Capybara.app_host    = "http://#{ENV.fetch('CAPYBARA_APP_HOST', 'host.docker.internal')}:#{Capybara.server_port}"
+  Capybara.always_include_port = true
+else
+  setup_with_selenium_manager
+  Capybara.default_driver = :firefox_headless
+  Capybara.javascript_driver = :firefox_headless
+end
+
 Capybara.disable_animation = true
 
 RSpec.configure do |config|
