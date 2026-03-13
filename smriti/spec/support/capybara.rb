6d59d7ef7b9528c31b13737c045d7e0c000bf685
Nitesh Purohit <nitesh.purohit.it@gmail.com>
2026-03-12 21:50:34 -0400

    refactor: streamline Capybara configuration for E2E tests
    # Description
    This commit refactors the Capybara configuration for end-to-end (E2E) tests to improve readability and maintainability. The changes ensure that the configuration is consistently applied when the `SELENIUM_REMOTE_URL` environment variable is present.
    
    # Related Issue
    N/A
    
    # Proposed Changes
    - Adjusted indentation for clarity in the E2E configuration block.
    - Ensured consistent application of Capybara settings when using remote Firefox driver.
    
    # Screenshots or Additional Information
    N/A

--- smriti/spec/support/capybara.rb
+++ smriti/spec/support/capybara.rb
@@ -49,12 +49,12 @@ end
 
 if ENV['E2E'] == '1'
   if ENV['SELENIUM_REMOTE_URL'].present?
-  Capybara.default_driver = :remote_firefox
-  Capybara.javascript_driver = :remote_firefox
-  Capybara.server_host = '0.0.0.0'
-  Capybara.server_port = ENV.fetch('CAPYBARA_PORT', '3000').to_i
-  Capybara.app_host    = "http://#{ENV.fetch('CAPYBARA_APP_HOST', 'host.docker.internal')}:#{Capybara.server_port}"
-  Capybara.always_include_port = true
+    Capybara.default_driver = :remote_firefox
+    Capybara.javascript_driver = :remote_firefox
+    Capybara.server_host = '0.0.0.0'
+    Capybara.server_port = ENV.fetch('CAPYBARA_PORT', '3000').to_i
+    Capybara.app_host    = "http://#{ENV.fetch('CAPYBARA_APP_HOST', 'host.docker.internal')}:#{Capybara.server_port}"
+    Capybara.always_include_port = true
   else
     setup_with_selenium_manager
     Capybara.default_driver = :firefox_headless
